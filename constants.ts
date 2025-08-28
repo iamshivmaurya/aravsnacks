0713f2fb489a4fd4e9c3ecfd77b77ab3cc7352a7
shiv <skmaurya.ald@gmail.com>
2025-08-28 22:40:11 +0530

    New package added

--- constants.ts
+++ constants.ts
@@ -1,6 +1,7 @@
-// constants.ts
-export const API_BASE_URL = 'http://127.0.0.1:8000';
 
+// constants.ts
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:8000";
+console.log("=====================API_BASE_URL================", API_BASE_URL);
 export const GET_PRODUCTS_API = `${API_BASE_URL}/products?skip=0&limit=100`;
 export const GET_QUOTES_API = `${API_BASE_URL}/quotes`;
 export const GET_CUSTOMERS_API = `${API_BASE_URL}/customers`;
